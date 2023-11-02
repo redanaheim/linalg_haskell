ef46c9ce7300991cd021e3f997638fea6c708aaf
TigerGold59 <54746609+redanaheim@users.noreply.github.com>
2023-11-01 21:24:41 -0400

    implement qr factorization

--- Normed.hs
+++ Normed.hs
@@ -8,6 +8,7 @@ import Vector
 import Solvable
 import Polynomial
 import Expression hiding (main)
+import Matrix
 
 class (Vector a, RealField (Scalar a)) => NormedSpace a where
     norm :: a -> Scalar a
@@ -21,7 +22,16 @@ normalize x = Vector.fmul x (Field.fminv (norm x))
 gram_schmidt :: (InnerProduct a, NormedSpace a) => [a] -> [a]
 gram_schmidt x = foldr (\x subspace_basis -> subspace_basis ++ [normalize (Vector.vadd x (Vector.vneg (project subspace_basis x)))]) [] (reverse x)
 
+-- q is the first matrix, r is the second
+qr_factorize :: (Field a, InnerProduct [a], NormedSpace [a]) => Matrix a -> (Matrix a, Matrix a)
+qr_factorize m = do
+    let q = risky_columns (gram_schmidt (content (transpose m)))
+    let r = change_of_basis_matrix (content (transpose m)) (content (transpose q))
+    (q, r)
+
 main = do
-    let basis = [[RationalConst (-2), RationalConst (-3), RationalConst 2], [RationalConst 0, RationalConst (-1), RationalConst 1]] :: [[Expression]]
-    print (gram_schmidt basis)
-    print (fmap (fmap (compute (ComputationContext (const 1)))) (gram_schmidt basis))
+    let basis = [[-2, -1, 1], [-2, -5, 7], [-2, -1, -5], [-2, -5, 1]] :: [[Double]]
+    let mat = Matrix 4 3 basis
+    print mat
+    print (fst (qr_factorize mat))
+    print (snd (qr_factorize mat))
